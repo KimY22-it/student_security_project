cd47a63dd96932810afc1d1f9788c484a7985425
KimY <kimy2212004@gmail.com>
2026-03-23 15:49:05 +0700

    feat: update database setup

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -3,7 +3,7 @@ USE student_db;
 CREATE TABLE accounts (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') NOT NULL DEFAULT 'user'
 );
 

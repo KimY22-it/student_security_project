8da92ad746c7b0d021d07d9928cf2ecfd5eaae92
KimY <kimy2212004@gmail.com>
2026-03-23 10:07:13 +0700

    feat: add masking functions

--- masking.py
+++ masking.py
@@ -1,11 +1,17 @@
 def mask_email(email):
-    return email
+    at_index =  email.find('@')
+    if at_index <= 2:
+        return at_index * '*' + email[at_index:]
+
+    return email[:2] + (at_index - 2) * '*' + email[at_index:]
 
 def mask_phone(phone):
-    return phone
+    return phone[:3] + (len(phone) - 5) * '*' + phone[-2:]
 
 def mask_cccd(cccd):
-    return cccd
+    return cccd[:3] + (len(cccd) - 5) * '*' + cccd[-2:]
 
 def mask_address(address):
-    return address
+    if len(address) <= 6:
+        return len(address) * '*'
+    return address[:6] + (len(address) - 6) * '*'

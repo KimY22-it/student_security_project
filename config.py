ffcb29370cd20ff0d2ab21886947c283268e3d6c
KimY <kimy2212004@gmail.com>
2026-03-26 08:15:43 +0700

    fix: update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "1234",
-    "database": "student_db"
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME")
 }
-APP_SECRET_KEY = "abcdefghaes12345"
+APP_SECRET_KEY = os.getenv("KEY")
